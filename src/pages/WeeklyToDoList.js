b6caaeacf5fd6aec822f0423f82232ea3106ee1a
Your Name <you@example.com>
2022-12-18 13:33:27 +0200

    new

--- src/pages/WeeklyToDoList.js
+++ src/pages/WeeklyToDoList.js
@@ -1,19 +1,21 @@
 import { WeeklyToDoListTable } from "../components/WeeklyToDoList/WToDoListTable";
 import { MonthCalendarHeader } from "../components/MonthCalendar/MonthCalendarHeader";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import moment from "moment";
-import { RepeatMessage } from '../components/Event/RepeatMessage';
-import { AllocationMessage } from '../components/DailyToDoList/AllocationMessage';
+
+
 
 
 
 const WeeklyToDoList = () => {
 
-     moment.updateLocale('en', { week: { dow: 1 } });
+     moment.updateLocale('en', {week:{dow:1}});
      const [today, setToday] = useState(moment());
      const startDay = today.clone().startOf('week');
      const [tasks, setTasks] = useState({});
-
+     const [activateDel, setActivateDel] = useState(false);
+     const [activateEdit, setActivateEdit] = useState(false);
+     const [currTask, setCurrTask]=useState(null);
     
     const prevHandler = () => {
          console.log('prev');
@@ -27,8 +29,17 @@ const WeeklyToDoList = () => {
    function getTasks() {
         return tasks;
    }
+   function addTask(e) {
+        const task_list = moment(e.dateOfTask).format('DDMMYYYY') in tasks ? tasks[moment(e.dateOfTask).format('DDMMYYYY')] : [];
+        task_list.push(e);
+        setTasks({
+             ...tasks,
+             [moment(e.dateOfTask).format('DDMMYYYY')]: task_list
+        });
+       
+   }
 
-
+  
    const handleCheck = (id, date) =>{
      const listTasks = tasks[date].map((task) => task.id === id ? {...task, checked: !task.checked } : task);
      setTasks({
@@ -38,50 +49,42 @@ const WeeklyToDoList = () => {
      
    }
 
+     const editStatus = (a) => {
+          setActivateEdit(a);
 
-     const [showAllocationMessage, setShowAllocationMessage] = useState(false);
-
-    
-     const [tempTask, setTempTask] = useState({})
-     function addTask(e) {
-          const task_list = moment(e.dateOfTask).format('DDMMYYYY') in tasks ? tasks[moment(e.dateOfTask).format('DDMMYYYY')] : [];
-          task_list.push(e);
-          setTasks({
-               ...tasks,
-               [moment(e.dateOfTask).format('DDMMYYYY')]: task_list
-          });
-          console.log(tasks);
-         
      }
 
+     const delStatus = (a) => {
+          setActivateDel(a);
 
+     }
+
+   
      return (
-          <div style={{ 'margin-top': '10%' }}>
-               <AllocationMessage
-                    text={'Your task will be auto allocated in available time before deadline'}
-                    showMessage={showAllocationMessage}
-                    setShowMessage={setShowAllocationMessage} />
-
-               <MonthCalendarHeader
-                    today={today}
-                    prevHandler={prevHandler}
-                    nextHandler={nextHandler}
-                    currCalendar="week"
-                    addTask={addTask}
-                    setShowAllocationMessage={setShowAllocationMessage}
-                    />
-
-               <WeeklyToDoListTable
-                    tasks={getTasks}
-                    handleCheck={handleCheck}
-                    today={today}
-                    startDay={startDay} />
-          </div>
-
-
+       <div style={{ 'margin-top':'10%'}}> 
+       {console.log(currTask)}
+                  <MonthCalendarHeader
+                       today={today} 
+                       prevHandler={prevHandler} 
+                       nextHandler={nextHandler}
+                       currCalendar="week"
+                       addTask={addTask}
+                       />
+                       
+                  <WeeklyToDoListTable
+                       tasks={getTasks}
+                       handleCheck={handleCheck}
+                       today={today}
+                       startDay={startDay}
+                       currTask={currTask}
+                       setCurrTask={setCurrTask}
+                       editStatus={(a) => editStatus(a)}
+                       delStatus={(a) => delStatus(a)}/> 
+       </div>
+   
      )
-
-};
+   
+}; 
 
 
 export { WeeklyToDoList };
