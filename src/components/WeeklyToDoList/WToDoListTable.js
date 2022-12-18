c89b2b35efa9aea1ca1f8760b52c9c144c29dacc
Your Name <you@example.com>
2022-12-18 13:34:18 +0200

    new

--- src/components/WeeklyToDoList/WToDoListTable.js
+++ src/components/WeeklyToDoList/WToDoListTable.js
@@ -16,9 +16,12 @@ const WeeklyToDoListTable = (props) => {
             tasks={dayItem.format('DDMMYYYY') in props.tasks() ? props.tasks()[dayItem.format('DDMMYYYY')] : []}
             key = {dayItem.format('DDMMYYYY')}
             date = {dayItem.format('DDMMYYYY')}
+            dayItem={dayItem}
             handleCheck={props.handleCheck}
             weekday = {dayItem.format('dddd')}
             daynum = {dayItem.format('D')}
+            currTask={props.currTask}
+            setCurrTask={props.setCurrTask}
             />
           
       ))
